ff7da199d7c7d1dc8165e927ae759d8149b8aacd
paulo <paulo@LAPTOP-C1SOC6DK>
2022-07-07 15:23:46 -0300

    Segundo commit

--- POO java Dio/src/br/com/dio/desafio/dominio/Mentoria.java
+++ POO java Dio/src/br/com/dio/desafio/dominio/Mentoria.java
@@ -2,10 +2,9 @@ package br.com.dio.desafio.dominio;
 
 import java.time.LocalDate;
 
-public class Mentoria extends Conteudo {	
-	private LocalDate data;//Encapsulado
+public class Mentoria extends Conteudo {
 	
-	
+	private LocalDate data;//Encapsulado	
 	
 	@Override
 	public double calcularXp() {
